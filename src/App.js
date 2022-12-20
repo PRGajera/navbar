1c5890e14be9c99f13ca60bfa856210a2fec3941
drashtikalathiya05 <drashtikalthiya05@gmail.com>
2022-12-20 12:13:19 +0530

    css begin

--- src/App.js
+++ src/App.js
@@ -6,44 +6,45 @@ import { CgProfile } from 'react-icons/cg';
 
 function App() {
   return (
-    <nav className='container'>
-      <div className='logo'>
-        <a href='#'> <FaCube />Untitle UI </a>
-        </div>
-        <div className='maintabs'>
-          <ul className='tabs'>
-            <li className='name'>
-              <a href='#'>Home</a>
-            </li>
-            <li className='name'><a href='#'>Products</a>
-              <ul className='subtabs'>
-                <li className='subname'><a href='#'>A</a></li>
-                <li className='subname'><a href='#'>B</a></li>
-                <li className='subname'><a href='#'>C</a></li>
-                <li className='subname'><a href='#'>D</a></li>
+    <nav className='navbar'>
+      <div className='block'>
+        <a href='#' className='logo'> <FaCube />Untitle UI </a>
+      </div>
+      <div className='maintabs'>
+        <ul className='tabs'>
+          <li className='name'>
+            <a href='#' className='deco'>Home</a>
+          </li>
+          <li className='name'><a href='#'className='deco'>Products</a>
+            <ul className='subtabs'>
+              <li className='subname'><a href='#'className='deco'>A</a></li>
+              <li className='subname'><a href='#'className='deco'>B</a></li>
+              <li className='subname'><a href='#'className='deco'>C</a></li>
+              <li className='subname'><a href='#'className='deco'>D</a></li>
             </ul>
-            </li>
-        
-        
-        <li className='name'><a href='#'>Resource</a>
-          <ul className='subtabs'>
+          </li>
 
-            <li className='subname'><a href='#'>1</a></li>
-            <li className='subname'><a href='#'>2</a></li>
-        <li className='subname'><a href='#'>3</a></li>
-      <li className='subname'><a href='#'>4</a></li>
-        
-      </ul >
-      </li>
-      <li className='name'><a href='#'>Pricing</a></li>
-      <li className='name'><a href='#'>About</a></li>
-    </ul >
-</div>
-    <div className='profile'>
-      <a href='#'><CgProfile/></a>
-    </div>
 
-</nav >
+          <li className='name'><a href='#'className='deco'>Resource</a>
+            <ul className='subtabs'>
+
+              <li className='subname'><a href='#'className='deco'>1</a></li>
+              <li className='subname'><a href='#'className='deco'>2</a></li>
+              <li className='subname'><a href='#'className='deco'>3</a></li>
+              <li className='subname'><a href='#'className='deco'>4</a></li>
+
+            </ul >
+          </li>
+          <li className='name'><a href='#'className='deco'>Pricing</a></li>
+          <li className='name'><a href='#'className='deco'>About</a></li>
+        </ul >
+      </div>
+      <div className='profile'>
+        <a href='#'><CgProfile /></a>
+      </div>
+
+
+    </nav >
   );
 }
 

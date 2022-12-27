f0bfae58953ade3de734a0903405df1c2d393078
drashtikalathiya05 <drashtikalthiya05@gmail.com>
2022-12-27 12:05:55 +0530

    Restart all

--- src/App.js
+++ src/App.js
@@ -11,39 +11,27 @@ function App() {
         <a href='#' className='logo'> <FaCube />Untitle UI </a>
         <div className='maintabs'>
           <ul className='tabs'>
-            <li className='name1'>
+            <li className='name'>
               <a href='#' className='deco'>Home</a>
             </li>
-            <li className='name2'><a href='#' className='deco'>Products</a>
-              {/* <ul className='subtabs'>
-              <li className='subname'><a href='#'className='deco'>A</a></li>
-              <li className='subname'><a href='#'className='deco'>B</a></li>
-              <li className='subname'><a href='#'className='deco'>C</a></li>
-              <li className='subname'><a href='#'className='deco'>D</a></li>
-            </ul> */}
+            <li className='name'>
+              <a href='#' className='deco'>Products</a>
             </li>
-
-
-            <li className='name3'><a href='#' className='deco'>Resource</a>
-              {/* <ul className='subtabs'>
-
-                <li className='subname'><a href='#'className='deco'>1</a></li>
-                <li className='subname'><a href='#'className='deco'>2</a></li>
-                <li className='subname'><a href='#'className='deco'>3</a></li>
-                <li className='subname'><a href='#'className='deco'>4</a></li>
-
-                </ul > */}
+            <li className='name'>
+              <a href='#' className='deco'>Resource</a>
             </li>
-            <li className='name4'><a href='#' className='deco'>Pricing</a></li>
-            <li className='name5'><a href='#' className='deco'>About</a></li>
+            <li className='name'>
+              <a href='#' className='deco'>Pricing</a>
+              </li>
+            <li className='name'>
+              <a href='#' className='deco'>About</a>
+              </li>
           </ul >
           <div className='profile'>
-            <a href='#'><CgProfile /></a>
+            {/* <a href='#'><CgProfile /></a> */}
           </div>
-
         </div>
       </div>
-
     </nav >
   );
 }
